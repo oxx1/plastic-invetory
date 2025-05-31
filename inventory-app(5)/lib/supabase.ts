06d1f485488349df2a85a5c87bc2fb5ed4500d9e
Tjorvii <karatemyran@gmail.com>
2025-05-31 22:39:24 +0200

    Delete all files

--- inventory-app(5)/lib/supabase.ts
+++
@@ -1,7 +0,0 @@
-import { createClient } from "@supabase/supabase-js"
-
-// Create a Supabase client
-export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL || "https://your-project-url.supabase.co",
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "your-anon-key",
-)
